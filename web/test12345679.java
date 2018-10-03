54684232fabf5188395e2f6180e72c78219637f2
philipmayer <philipmayer777@gmail.com>
2018-10-03 15:37:34 +0200

    Die das do

--- web/test12345679.java
+++ web/test12345679.java
@@ -1,3 +1,6 @@
+
+import static java.time.Clock.system;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,10 @@
  * @author Philip Mayer
  */
 public class test12345679 {
-    system.out.println("Hallo");
+    int i = 1;
+    int k = 2;
+    
+    message.show();
+    
+    
 }
